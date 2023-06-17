06e63c6051649bef5e0015ff94122d0613273904
Ha Jun Soo <jsooha7@gmail.com>
2023-06-17 22:33:52 +0900

    로그인 회원가입

---
+++ source/app/src/main/java/com/example/foruplus/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.foruplus
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        /*
+        tv_id = findViewById(R.id.tv_id);
+        tv_pass = findViewById(R.id.tv_pass);
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPass = intent.getStringExtrea("userPass");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPass);
+        */
+    }
+}
